2da069a854f51f135b3244eb09b6e1fc2aade439
Arjun Rajan <arjunrajan991@gmail.com>
2025-03-20 00:47:07 +0530

    board draw

--- main.cpp
+++ main.cpp
@@ -1,42 +1,50 @@
 #include <iostream>
 #include <ncurses.h>
+#include <vector>
 
 using namespace std;
 
 void draw_board() {
+    // origin: 5,10
+    vector<int> origin = {5, 10};
 
+    int height = 3;
+    int width = height * 3;
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            mvvline(origin[0] + (j * height), origin[1] + width + (i * width), ACS_VLINE, height);
+            mvhline(origin[0] + height + (i * height), origin[1] + (j * width), ACS_HLINE, width);
+        }
+    }
+
+    //for (int i = 0; i < 2; i++) {
+    //}
 }
 
 int main() {
-
     initscr();
     noecho();
     keypad(stdscr, TRUE);
     mousemask(BUTTON1_CLICKED, NULL);
     nodelay(stdscr, TRUE);
 
-    int ch;
-
     MEVENT event;
+    draw_board();
 
     while (1) {
-        clear();
-
-        ch = getch();
+        int ch = getch();
 
         if (ch == 'q') break;
-
         if (ch == KEY_MOUSE && getmouse(&event) == OK) {
-            if (event.bstate & BUTTON1_CLICKED) {
-                mvprintw(event.y, event.x, "X");
-                refresh();
-            }
+              if (event.bstate & BUTTON1_CLICKED) {
+                  mvprintw(event.y, event.x, "X");
+                  refresh();
+                }
         }
         refresh();
         napms(10);
     }
-
     endwin();
     return 0;
-
 }
