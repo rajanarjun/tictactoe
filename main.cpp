c157c33a4422230579625febfe3fe8b27c286f0e
Arjun Rajan <arjunrajan991@gmail.com>
2025-03-21 01:13:12 +0530

    added window with board inside. also saving intersection points

--- main.cpp
+++ main.cpp
@@ -4,36 +4,55 @@
 
 using namespace std;
 
-void draw_board() {
-    // origin: y = 5, x = 10
-    vector<int> origin = {5, 10};
+void draw_board(WINDOW *win) {
+    vector<int> boardOrigin = {1, 1};
+    vector<vector<int>> intersections;
 
-    int height = 3;
-    int width = height * 3;
+    int cell_height = 3;
+    int cell_width = cell_height * 3;
 
     for (int i = 1; i < 3; i++) {
-        mvvline(origin[0], origin[1] + (i * width), ACS_VLINE, (3 * height) + 1);
+        mvwvline(win, boardOrigin[0], boardOrigin[1] + (i * cell_width), ACS_VLINE, (3 * cell_height) + 1);
     }
     for (int j = 1; j < 3; j++) {
-        mvhline(origin[0] + (j * height), origin[1], ACS_HLINE, (3 * width) + 1);
+        mvwhline(win, boardOrigin[0] + (j * cell_height), boardOrigin[1], ACS_HLINE, (3 * cell_width) + 1);
     }
 
     for (int i = 1; i < 3; i++) {
         for (int j = 1; j < 3; j++) {
-            mvaddch(origin[0] + (i * height), origin[1] + (j * width), ACS_PLUS);
+            int y_loc = boardOrigin[0] + (i * cell_height);
+            int x_loc = boardOrigin[1] + (j * cell_width);
+            intersections.push_back({y_loc, x_loc});
+            mvwaddch(win, y_loc, x_loc, ACS_PLUS);
+        }
+    }
+    
+    int text_height = 20;
+    for (int i = 0; i < intersections.size(); i++) {
+        for (int j = 0; j < intersections[i].size(); j++) {
+            mvprintw(text_height, 10, "%d", intersections[i][j]);
+            text_height++;
         }
     }
 }
 
 int main() {
+    int win_height = 12;
+    int win_width = 30;
+    int click_x, click_y;
+
     initscr();
     noecho();
+    
+    WINDOW* main_win = newwin(win_height, win_width, 5, 10);
+    box(main_win, '|', '-');
+    draw_board(main_win);
+
     keypad(stdscr, TRUE);
     mousemask(BUTTON1_CLICKED, NULL);
     nodelay(stdscr, TRUE);
 
     MEVENT event;
-    draw_board();
 
     while (1) {
         int ch = getch();
@@ -41,10 +60,13 @@ int main() {
         if (ch == 'q') break;
         if (ch == KEY_MOUSE && getmouse(&event) == OK) {
               if (event.bstate & BUTTON1_CLICKED) {
+                  click_x = event.x;
+                  click_y = event.y;
                   mvprintw(event.y, event.x, "X");
                   refresh();
                 }
         }
+        wrefresh(main_win);
         refresh();
         napms(10);
     }
