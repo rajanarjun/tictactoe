aee7b18947e362754ec2f7177a52c4a503ae7aca
Arjun Rajan <arjunrajan991@gmail.com>
2025-03-22 23:47:34 +0530

    confined mousclicks to only board window

--- main.cpp
+++ main.cpp
@@ -27,47 +27,54 @@ void draw_board(WINDOW *win) {
         }
     }
     
-    int text_height = 20;
-    for (int i = 0; i < intersections.size(); i++) {
-        for (int j = 0; j < intersections[i].size(); j++) {
-            mvprintw(text_height, 10, "%d", intersections[i][j]);
-            text_height++;
-        }
-    }
+    //int text_height = 20;
+    //for (int i = 0; i < intersections.size(); i++) {
+    //    for (int j = 0; j < intersections[i].size(); j++) {
+    //        mvprintw(text_height, 10, "%d", intersections[i][j]);
+    //        text_height++;
+    //    }
+    //}
 }
 
 int main() {
     int win_height = 12;
     int win_width = 30;
-    int click_x, click_y;
+    int click_x, click_y, ch;
+
+    vector<int> scr_win_left_upper_corner = {5, 10};
+    vector<int> scr_win_right_lower_corner = {scr_win_left_upper_corner[0] + win_height - 1, scr_win_left_upper_corner[1] + win_width - 1};
 
     initscr();
     noecho();
+    cbreak();
     
-    WINDOW* main_win = newwin(win_height, win_width, 5, 10);
-    box(main_win, '|', '-');
-    draw_board(main_win);
-
     keypad(stdscr, TRUE);
     mousemask(BUTTON1_CLICKED, NULL);
     nodelay(stdscr, TRUE);
 
-    MEVENT event;
+    WINDOW* main_win = newwin(win_height, win_width, 5, 10);
+    box(main_win, '|', '-');
+    draw_board(main_win);
+    wrefresh(main_win);
 
+    keypad(main_win, TRUE);
+    nodelay(main_win, TRUE);
+
+    MEVENT event;
     while (1) {
-        int ch = getch();
+        ch = wgetch(main_win);
 
         if (ch == 'q') break;
         if (ch == KEY_MOUSE && getmouse(&event) == OK) {
               if (event.bstate & BUTTON1_CLICKED) {
-                  click_x = event.x;
-                  click_y = event.y;
-                  mvprintw(event.y, event.x, "X");
-                  refresh();
-                }
+                  click_x = event.x - 10;
+                  click_y = event.y - 5;
+                  //mvprintw(30, 10, "x: %d", click_x);
+                  //mvprintw(30, 20, "y: %d", click_y);
+                  mvwprintw(main_win, click_y, click_x, "X");
+                  wrefresh(main_win);
+              }
         }
-        wrefresh(main_win);
-        refresh();
         napms(10);
     }
     endwin();
