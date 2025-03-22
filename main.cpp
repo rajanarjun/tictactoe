d9f5a0d71787c28a8e4197aef2799c19fa1fd788
Arjun Rajan <arjunrajan991@gmail.com>
2025-03-23 03:09:41 +0530

    refactored coords that were in vectors to separate x and y ints

--- main.cpp
+++ main.cpp
@@ -14,40 +14,42 @@ static int cell_7_marked = 0;
 static int cell_8_marked = 0;
 static int cell_9_marked = 0;
 
-void draw_board(WINDOW *win, vector<int> &boardOrigin, int cell_height, int cell_width, vector<vector<int>> &intersections) {
+void draw_board(WINDOW *win, int boardOriginY, int boardOriginX, int cell_height, int cell_width, vector<vector<int>> &intersections) {
 
     for (int i = 1; i < 3; i++) {
-        mvwvline(win, boardOrigin[0], boardOrigin[1] + (i * cell_width), ACS_VLINE, (3 * cell_height) + 1);
+        mvwvline(win, boardOriginY, boardOriginX + (i * cell_width), ACS_VLINE, (3 * cell_height) + 1);
     }
     for (int j = 1; j < 3; j++) {
-        mvwhline(win, boardOrigin[0] + (j * cell_height), boardOrigin[1], ACS_HLINE, (3 * cell_width) + 1);
+        mvwhline(win, boardOriginY + (j * cell_height), boardOriginX, ACS_HLINE, (3 * cell_width) + 1);
     }
 
     for (int i = 1; i < 3; i++) {
         for (int j = 1; j < 3; j++) {
-            int y_loc = boardOrigin[0] + (i * cell_height);
-            int x_loc = boardOrigin[1] + (j * cell_width);
+            int y_loc = boardOriginY + (i * cell_height);
+            int x_loc = boardOriginX + (j * cell_width);
             intersections.push_back({y_loc, x_loc});
             mvwaddch(win, y_loc, x_loc, ACS_PLUS);
         }
     }
     
-    int text_height = 20;
-    for (int i = 0; i < intersections.size(); i++) {
-        for (int j = 0; j < intersections[i].size(); j++) {
-            mvprintw(text_height, 10, "%d", intersections[i][j]);
-            text_height++;
-            refresh();
-        }
-    }
+    //int text_height = 20;
+    //for (int i = 0; i < intersections.size(); i++) {
+    //    for (int j = 0; j < intersections[i].size(); j++) {
+    //        mvprintw(text_height, 10, "%d", intersections[i][j]);
+    //        text_height++;
+    //        refresh();
+    //    }
+    //}
 }
 
-void mark_input(WINDOW *win, int wy, int wx, vector<int> &boardOrigin, int cell_height, int cell_width, vector<vector<int>> &intersections) {
+void mark_input(WINDOW *win, int wy, int wx, int boardOriginY, int boardOriginX, int cell_height, int cell_width, vector<vector<int>> &intersections) {
     
     // cell 1
-    if (wy > boardOrigin[0] && wy < boardOrigin[0] + cell_height && wx > boardOrigin[1] && wx < boardOrigin[1] + cell_width) {
+    if (wy > boardOriginY && wy < boardOriginY + cell_height && wx > boardOriginX && wx < boardOriginX + cell_width) {
         if (cell_1_marked == 0) {
-            mvwprintw(win, wy, wx, "X");
+            int put_y = (cell_height + boardOriginY) / 2;
+            int put_x = (cell_width + boardOriginX) / 2;
+            mvwprintw(win, put_y, put_x, "X");
             cell_1_marked = 1;
         }
     }
@@ -67,10 +69,11 @@ void mark_input(WINDOW *win, int wy, int wx, vector<int> &boardOrigin, int cell_
 int main() {
     int win_height = 12;
     int win_width = 30;
-    vector<int> scr_win_left_upper_corner = {5, 10};
-    vector<int> scr_win_right_lower_corner = {scr_win_left_upper_corner[0] + win_height - 1, scr_win_left_upper_corner[1] + win_width - 1};
+    int win_originY = 10;
+    int win_originX = 10;
 
-    vector<int> board_origin = {1, 1};
+    int board_originY = 1;
+    int board_originX = 1;
     int cell_height = 3;
     int cell_width = cell_height * 3;
     vector<vector<int>> intersections;
@@ -85,9 +88,18 @@ int main() {
     mousemask(BUTTON1_CLICKED, NULL);
     nodelay(stdscr, TRUE);
 
-    WINDOW* main_win = newwin(win_height, win_width, 5, 10);
+    mvprintw(0, 0, "%s", R"(
+ _   _           _                  _             
+| |_(_) ___     | |_ __ _  ___     | |_ ___   ___ 
+| __| |/ __|____| __/ _` |/ __|____| __/ _ \ / _ \
+| |_| | (_|_____| || (_| | (_|_____| || (_) |  __/
+ \__|_|\___|     \__\__,_|\___|     \__\___/ \___|
+ )");
+    refresh();
+                                                  
+    WINDOW* main_win = newwin(win_height, win_width, win_originY, win_originX);
     box(main_win, '|', '-');
-    draw_board(main_win, board_origin, cell_height, cell_width, intersections);
+    draw_board(main_win, board_originY, board_originX, cell_height, cell_width, intersections);
     wrefresh(main_win);
 
     keypad(main_win, TRUE);
@@ -99,9 +111,9 @@ int main() {
         if (ch == 'q') break;
         if (ch == KEY_MOUSE && getmouse(&event) == OK) {
               if (event.bstate & BUTTON1_CLICKED) {
-                  click_x = event.x - 10;
-                  click_y = event.y - 5;
-                  mark_input(main_win, click_y, click_x, board_origin, cell_height, cell_width, intersections);
+                  click_y = event.y - win_originY;
+                  click_x = event.x - win_originX;
+                  mark_input(main_win, click_y, click_x, board_originY, board_originX, cell_height, cell_width, intersections);
               }
         }
         napms(10);
