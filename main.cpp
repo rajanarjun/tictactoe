ba7a1f0e19aa2549b47557a47c5869bfbdbb831a
Arjun Rajan <arjunrajan991@gmail.com>
2025-03-25 23:56:34 +0530

    using strings for player char, using strings for tracking marked cell

--- main.cpp
+++ main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <ncurses.h>
 #include <vector>
+#include <string>
 
 using namespace std;
 
-static int cell_1_marked = 0;
-static int cell_2_marked = 0;
-static int cell_3_marked = 0;
-static int cell_4_marked = 0;
-static int cell_5_marked = 0;
-static int cell_6_marked = 0;
-static int cell_7_marked = 0;
-static int cell_8_marked = 0;
-static int cell_9_marked = 0;
+static string cell_1_marked = "";
+static string cell_2_marked = "";
+static string cell_3_marked = "";
+static string cell_4_marked = "";
+static string cell_5_marked = "";
+static string cell_6_marked = "";
+static string cell_7_marked = "";
+static string cell_8_marked = "";
+static string cell_9_marked = "";
 
 void draw_board(WINDOW *win, int boardOriginY, int boardOriginX, int cell_height, int cell_width, vector<vector<int>> &intersections) {
 
@@ -42,84 +43,92 @@ void draw_board(WINDOW *win, int boardOriginY, int boardOriginX, int cell_height
     //}
 }
 
-void mark_input(WINDOW *win, int wy, int wx, int boardOriginY, int boardOriginX, int cell_height, int cell_width, vector<vector<int>> &intersections, char *ch) {
+void mark_input(WINDOW *win, int wy, int wx, int boardOriginY, int boardOriginX, int cell_height, int cell_width, vector<vector<int>> &intersections, const string &ch) {
     
     int put_y = (cell_height + boardOriginY) / 2;
     int put_x = (cell_width + boardOriginX) / 2;
     // cell 1
     if (wy > boardOriginY && wy < boardOriginY + cell_height && wx > boardOriginX && wx < boardOriginX + cell_width) {
-        if (cell_1_marked == 0) {
-            mvwprintw(win, put_y, put_x, "%s", ch);
-            cell_1_marked = 1;
+        if (cell_1_marked.empty()) {
+            mvwprintw(win, put_y, put_x, "%s", ch.c_str());
+            cell_1_marked = ch;
         }
     }
 
     // cell 2
     if (wy > boardOriginY && wy < boardOriginY + cell_height && wx > boardOriginX + cell_width && wx < boardOriginX + (cell_width*2)) {
-        if (cell_2_marked == 0) {
-            mvwprintw(win, put_y, put_x + cell_width, "%s", ch);
-            cell_2_marked = 1;
+        if (cell_2_marked.empty()) {
+            mvwprintw(win, put_y, put_x + cell_width, "%s", ch.c_str());
+            cell_2_marked = ch;
         }
     }
 
     // cell 3
     if (wy > boardOriginY && wy < boardOriginY + cell_height && wx > boardOriginX + (cell_width*2) && wx < boardOriginX + (cell_width*3)) {
-        if (cell_3_marked == 0) {
-            mvwprintw(win, put_y, put_x + (cell_width*2), "%s", ch);
-            cell_3_marked = 1;
+        if (cell_3_marked.empty()) {
+            mvwprintw(win, put_y, put_x + (cell_width*2), "%s", ch.c_str());
+            cell_3_marked = ch;
         }
     }
 
     // cell 4
     if (wy > boardOriginY + cell_height && wy < boardOriginY + (cell_height*2) && wx > boardOriginX && wx < boardOriginX + cell_width) {
-        if (cell_4_marked == 0) {
-            mvwprintw(win, put_y + cell_height, put_x, "%s", ch);
-            cell_4_marked = 1;
+        if (cell_4_marked.empty()) {
+            mvwprintw(win, put_y + cell_height, put_x, "%s", ch.c_str());
+            cell_4_marked = ch;
         }
     }
 
     // cell 5
     if (wy > boardOriginY + cell_height && wy < boardOriginY + (cell_height*2) && wx > boardOriginX + cell_width && wx < boardOriginX + (cell_width*2)) {
-        if (cell_5_marked == 0) {
-            mvwprintw(win, put_y + cell_height, put_x + cell_width, "%s", ch);
-            cell_5_marked = 1;
+        if (cell_5_marked.empty()) {
+            mvwprintw(win, put_y + cell_height, put_x + cell_width, "%s", ch.c_str());
+            cell_5_marked = ch;
         }
     }
 
     // cell 6
     if (wy > boardOriginY + cell_height && wy < boardOriginY + (cell_height*2) && wx > boardOriginX + (cell_width*2) && wx < boardOriginX + (cell_width*3)) {
-        if (cell_6_marked == 0) {
-            mvwprintw(win, put_y + cell_height, put_x + (cell_width*2), "%s", ch);
-            cell_6_marked = 1;
+        if (cell_6_marked.empty()) {
+            mvwprintw(win, put_y + cell_height, put_x + (cell_width*2), "%s", ch.c_str());
+            cell_6_marked = ch;
         }
     }
 
     // cell 7
     if (wy > boardOriginY + (cell_height*2) && wy < boardOriginY + (cell_height*3) && wx > boardOriginX && wx < boardOriginX + cell_width) {
-        if (cell_7_marked == 0) {
-            mvwprintw(win, put_y + (cell_height*2) + 1, put_x, "%s", ch);
-            cell_7_marked = 1;
+        if (cell_7_marked.empty()) {
+            mvwprintw(win, put_y + (cell_height*2) + 1, put_x, "%s", ch.c_str());
+            cell_7_marked = ch;
         }
     }
 
     // cell 8
     if (wy > boardOriginY + (cell_height*2) && wy < boardOriginY + (cell_height*3) && wx > boardOriginX + cell_width && wx < boardOriginX + (cell_width*2)) {
-        if (cell_8_marked == 0) {
-            mvwprintw(win, put_y + (cell_height*2) + 1, put_x + cell_width, "%s", ch);
-            cell_8_marked = 1;
+        if (cell_8_marked.empty()) {
+            mvwprintw(win, put_y + (cell_height*2) + 1, put_x + cell_width, "%s", ch.c_str());
+            cell_8_marked = ch;
         }
     }
 
     // cell 9
     if (wy > boardOriginY + (cell_height*2) && wy < boardOriginY + (cell_height*3) && wx > boardOriginX + (cell_width*2) && wx < boardOriginX + (cell_width*3)) {
-        if (cell_9_marked == 0) {
-            mvwprintw(win, put_y + (cell_height*2) + 1, put_x + (cell_width*2), "%s", ch);
-            cell_9_marked = 1;
+        if (cell_9_marked.empty()) {
+            mvwprintw(win, put_y + (cell_height*2) + 1, put_x + (cell_width*2), "%s", ch.c_str());
+            cell_9_marked = ch;
         }
     }
 
     wrefresh(win);
 }
+
+//int check_result() {
+//    // check cell marked variable
+//    // 0 = draw
+//    // 1 = X wins
+//    // 2 = 0 wins
+//
+//}
 
 int main() {
     int win_height = 12;
@@ -134,7 +143,8 @@ int main() {
     vector<vector<int>> intersections;
 
     int click_x, click_y, ch;
-    char player_char[] = "O";
+    const string player_1_char = "X";
+    const string player_2_char = "O";
 
     initscr();
     noecho();
@@ -171,7 +181,7 @@ int main() {
               if (event.bstate & BUTTON1_CLICKED) {
                   click_y = event.y - win_originY;
                   click_x = event.x - win_originX;
-                  mark_input(main_win, click_y, click_x, board_originY, board_originX, cell_height, cell_width, intersections, player_char);
+                  mark_input(main_win, click_y, click_x, board_originY, board_originX, cell_height, cell_width, intersections, player_1_char);
               }
         }
         napms(10);
